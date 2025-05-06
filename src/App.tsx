48b7cc323b540ea75de82b8033c0929c8f937fd7
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-05-06 13:17:12 +0300

    anecdotes

--- src/App.tsx
+++ src/App.tsx
@@ -125,3 +125,61 @@ const App = () => {
 
 export default App;
 */
+/** anecdotes
+import { useState } from "react";
+const LargestVotes = (props) => {
+  const [votesLista, anecdoteLista] = props.neededProps;
+  let biggestNum = 0;
+  let indexOfBiggestNum = -1;
+  votesLista.forEach((element) => {
+    if (element >= biggestNum) {
+      biggestNum = element;
+      indexOfBiggestNum++;
+    }
+  });
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      {anecdoteLista[indexOfBiggestNum]}
+      <p>has {biggestNum} votes</p>
+    </div>
+  );
+};
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const showRan = () => {
+    const newSelected = Math.floor(Math.random() * anecdotes.length);
+    setSelected(newSelected);
+  };
+  const [votes, setVotes] = useState([1, 4, 6, 3]);
+  const voteFor = (selectedIndex) => {
+    const copy = [...votes];
+    copy[selectedIndex] += 1;
+    setVotes(copy);
+  };
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <p>Has {votes[selected]} votes</p>
+      <br />
+      <button onClick={() => voteFor(selected)}>Vote</button>
+      <button onClick={showRan}>Next Anecdote</button>
+      <LargestVotes neededProps={[votes, anecdotes]}></LargestVotes>
+    </div>
+  );
+};
+
+export default App;
+*/
